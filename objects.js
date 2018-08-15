66574b9ab8eb8e0110a225e812ac7305f20420a1
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 15:45:58 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = new Object({artistName: 'Rise Against'});
+var playlist = new Object({artistName: songTitle});
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName] = songTitle;
   Object.assign({}, playlist, {artistName: ['My Bloody Valentine', 'Phil Ochs', 'Slowdive'] })
