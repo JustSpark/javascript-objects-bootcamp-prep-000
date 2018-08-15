49b030d45798aa14f8001f7fea1701fae7e3210d
christopherbroussard44@yahoo.com <christopherbroussard44@yahoo.com>
2018-08-15 15:54:31 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -5,5 +5,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle});
 }
 function removeFromPlaylist(playlist, artistName) { playlist[artistName];
-delete 
+delete removeFromPlaylist
 }
